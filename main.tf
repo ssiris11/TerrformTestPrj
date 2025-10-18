d8e5fe5c59181881d0748220568247676d056346
siri <sireeshasunkara6@gmail.com>
2025-10-18 15:53:22 +0530

    updated backend strg name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-rg"
-    storage_account_name = "tfstateaks123"
+    storage_account_name = "tfstateakstest123"
     container_name       = "tfstate"
     key                  = "aks-demo.tfstate"
   }
