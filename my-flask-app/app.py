8d2944eafb14eb31e43672005e072a54f4bfc6fe
siri <sireeshasunkara6@gmail.com>
2025-11-17 11:50:14 +0530

    my first commit

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🎉 Hello from Flask running on Azure Kubernetes Service!"
+    return "🎉 Hello from Flask running on Azure Kubernetes Service! making cherry pick hands on"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
